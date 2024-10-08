7ab69c170b7948537190dcb66924469729180d29
joaopfonseca <jpfonseca@novaims.unl.pt>
2024-10-08 15:10:15 -0400

    REF organize codebase

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from ._loader import load_model
+from ._output_handler import generate_output, edit_output
+
+__all__ = ["load_model", "generate_output", "edit_output"]
--- src/_output_handler.py
+++ src/_output_handler.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 
 
-def generate_response(
+def generate_output(
     init_prompt,
     model,
     tokenizer,
@@ -48,23 +48,18 @@ def generate_response(
         {"role": "user", "content": init_prompt},
         {"role": "assistant", "content": ""},
     ]
-    prompt_ids = tokenizer.apply_chat_template(
-        prompt, return_tensors="pt"
-    )[0][:-1].reshape(1, -1)
+    prompt_ids = tokenizer.apply_chat_template(prompt, return_tensors="pt")[0][
+        :-1
+    ].reshape(1, -1)
 
-    while not (
-        output.find(TERMINATOR) >= 0
-        or output_ids.shape[-1] >= max_new_tokens
-    ):
+    while not (output.find(TERMINATOR) >= 0 or output_ids.shape[-1] >= max_new_tokens):
 
         if cuda:
             prompt_ids = prompt_ids.cuda()
             output_ids = output_ids.cuda()
 
         with torch.no_grad():
-            all_ids = torch.cat(
-                [prompt_ids, output_ids], dim=-1
-            )
+            all_ids = torch.cat([prompt_ids, output_ids], dim=-1)
             outputs = model(
                 all_ids,
                 use_cache=False,
@@ -107,9 +102,7 @@ def generate_response(
     return data
 
 
-def edit_output(
-    data: pd.DataFrame, token_pos: int, new_token: int
-) -> pd.DataFrame:
+def edit_output(data: pd.DataFrame, token_pos: int, new_token: int) -> pd.DataFrame:
     new_text = data.loc[token_pos, "texts"][new_token]
     data.loc[token_pos, "selected_idx"] = new_token
     data.loc[token_pos, "selected_text"] = new_text
