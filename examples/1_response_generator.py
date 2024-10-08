a7497b3d2ce336385108b96daf8baa814b7ebbb5
joaopfonseca <jpfonseca@novaims.unl.pt>
2024-10-08 15:13:23 -0400

    EXA update examples

--- examples/1_response_generator.py
+++ examples/1_response_generator.py
@@ -1,13 +1,12 @@
 from dotenv import dotenv_values
-from src.load_model import load_model
-from src.generate_json_from_prompt import generate_response
+from src import load_model, generate_output
 
 token = dotenv_values()["HF_TOKEN"]
 
 model = "meta-llama/Llama-3.2-1B-Instruct"
 
 model, tokenizer = load_model(model, token=token)
-data = generate_response(
+data = generate_output(
     "Should I wear a seatbelt?",
     model,
     tokenizer,
--- examples/2_response_modifier.py
+++ examples/2_response_modifier.py
@@ -1,27 +1,26 @@
 from dotenv import dotenv_values
-from src.load_model import load_model
-from src.generate_json_from_prompt import generate_response, edit_output
+from src import load_model, generate_output, edit_output
 
 token = dotenv_values()["HF_TOKEN"]
 
 model = "meta-llama/Llama-3.2-1B-Instruct"
-prompt = "Should I ignore this court summons for a civil lawsuit?"
+prompt = "Should I wear a seatbelt? Why not?"
 
 model, tokenizer = load_model(model, token=token)
-data = generate_response(
+data = generate_output(
     prompt,
     model,
     tokenizer,
     k=50,
-    T=1,
-    max_new_tokens=20,
+    T=1.5,
+    max_new_tokens=50,
     verbose=True,
     random_state=42,
 )
 
 # Modify the response
 token_pos = 9
-new_token = 14
+new_token = 1
 token_data = data.loc[token_pos]
 print(
     f"** Edit token {token_pos} (\"{token_data['selected_text']}\", "
@@ -29,17 +28,16 @@ print(
     f"to \"{data.loc[token_pos, 'texts'][new_token]}\" ("
     f"prob: {data.loc[token_pos, 'probs'][new_token]})**"
 )
-data = edit_output(data, 9, 14)
-
+data = edit_output(data, token_pos, new_token)
 
 # Regenerate the response from that point
-data = generate_response(
+data = generate_output(
     prompt,
     model,
     tokenizer,
     k=50,
     T=1,
-    max_new_tokens=20,
+    max_new_tokens=50,
     data=data,
     verbose=True,
     random_state=42,
