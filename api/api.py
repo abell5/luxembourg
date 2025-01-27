762d8aba29122e495145fd13feecbe1d3cc0b70d
joaopfonseca <jpfonseca@novaims.unl.pt>
2025-01-27 15:25:03 -0500

    ENH add endpoint for static generation

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ import pandas as pd
 import torch
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from . import load_model, generate_output_stream, edit_output
+from . import load_model, generate_output, generate_output_stream, edit_output
 
 CUDA = torch.cuda.is_available()
 MODEL_NAME = "meta-llama/Llama-3.2-1B-Instruct"
@@ -44,6 +44,38 @@ async def generate(
         data = pd.DataFrame(json.loads(data[0]))
 
     data = generate_output_stream(
+        init_prompt=init_prompt,
+        model=model,
+        tokenizer=tokenizer,
+        k=k,
+        T=T,
+        max_new_tokens=max_new_tokens,
+        cuda=CUDA,
+        verbose=verbose,
+        random_state=random_state,
+        data=data,
+    )
+    return StreamingResponse(data, media_type="application/json")
+
+
+@app.post("/generate_static", summary="Generate an output")
+async def generate(
+    init_prompt: str,
+    k: int = 30,
+    T: float = 1,
+    max_new_tokens: int = 100,
+    verbose: bool = False,
+    random_state: int = None,
+    data: list = None,
+):
+    """
+    Generate an output stream based on a prompt, using a LLM (Llama 3.2 1B Instruct).
+    """
+    if data is not None:
+        print(json.loads(data[0]))
+        data = pd.DataFrame(json.loads(data[0]))
+
+    data = generate_output(
         init_prompt=init_prompt,
         model=model,
         tokenizer=tokenizer,
