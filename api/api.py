06b4c5f0eba7c596cdf05e35c90ceca8de13435e
joaopfonseca <jpfonseca@novaims.unl.pt>
2025-06-03 16:58:50 -0400

    ENH add safenudge (hidden state version)

--- api/api.py
+++ api/api.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pickle
 from dotenv import dotenv_values
 import pandas as pd
 import torch
@@ -8,14 +9,15 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from . import (
     load_model,
-    generate_output,
     generate_output_stream,
     edit_output,
     parse_connected_json_objects,
 )
+from .safenudge import SafeNudge
 
 CUDA = torch.cuda.is_available()
-MODEL_NAME = "meta-llama/Llama-3.2-1B-Instruct"
+MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct"
+SAFENUDGE_CLF = pickle.load(open("api/artifacts/clf_mlp_hidden_states_truncated.pkl", "rb"))
 
 if "HF_TOKEN" in os.environ:
     TOKEN = os.environ["HF_TOKEN"]
@@ -51,56 +53,45 @@ async def generate(
     verbose: bool = False,
     random_state: int = None,
     data: list = None,
+    safenudge: bool = False,
 ):
     """
     Generate an output stream based on a prompt, using a LLM (Llama 3.2 1B Instruct).
     """
-    data = generate_output_stream(
-        init_prompt=init_prompt,
-        model=model,
-        tokenizer=tokenizer,
-        k=k,
-        T=T,
-        max_new_tokens=max_new_tokens,
-        sleep_time=sleep_time,
-        cuda=CUDA,
-        verbose=verbose,
-        random_state=random_state,
-        data=data,
-    )
-    return StreamingResponse(data, media_type="application/json")
-
-
-# @app.post("/generate_static", summary="Generate an output")
-# async def generate_static(
-#     init_prompt: str,
-#     k: int = 30,
-#     T: float = 1,
-#     max_new_tokens: int = 100,
-#     verbose: bool = False,
-#     random_state: int = None,
-#     data: list = None,
-# ):
-#     """
-#     Generate an output stream based on a prompt, using a LLM (Llama 3.2 1B Instruct).
-#     """
-#     if data is not None:
-#         print(json.loads(data[0]))
-#         data = pd.DataFrame(json.loads(data[0]))
-#
-#     data = generate_output(
-#         init_prompt=init_prompt,
-#         model=model,
-#         tokenizer=tokenizer,
-#         k=k,
-#         T=T,
-#         max_new_tokens=max_new_tokens,
-#         cuda=CUDA,
-#         verbose=verbose,
-#         random_state=random_state,
-#         data=data,
-#     )
-#     return StreamingResponse(data, media_type="application/json")
+    if not safenudge:
+        data = generate_output_stream(
+            init_prompt=init_prompt,
+            model=model,
+            tokenizer=tokenizer,
+            k=k,
+            T=T,
+            max_new_tokens=max_new_tokens,
+            sleep_time=sleep_time,
+            cuda=CUDA,
+            verbose=verbose,
+            random_state=random_state,
+            data=data,
+        )
+        return StreamingResponse(data, media_type="application/json")
+    else:
+        print("hello")
+        data = SafeNudge(
+            model=model,
+            tokenizer=tokenizer,
+            mode="topk",
+            k=k,
+            temperature=T,
+            random_state=random_state,
+            cuda=CUDA
+        ).generate_moderated(
+            prompt=init_prompt,
+            clf=SAFENUDGE_CLF,
+            target="",
+            tau=0.9,
+            max_tokens=max_new_tokens,
+            verbose=verbose
+        )
+        return StreamingResponse(data, media_type="application/json")
 
 
 def edit(
